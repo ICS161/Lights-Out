1660dea9676a373ba4cbd6f674a37d5ced219f06
ICS 161 <eaho1@uci.edu>
2019-01-31 20:55:04 -0800

    Started project with grid tiles

---
+++ Lights Out/Assets/Scripts/TileComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileComponent : MonoBehaviour
+{
+    public Material redMaterial, blueMaterial;
+
+    private MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        meshRenderer = this.GetComponent<MeshRenderer>();
+    }
+
+    void Start()
+    {
+        meshRenderer.material = blueMaterial;
+    }
+
+    void OnMouseDown()
+    {
+        meshRenderer.material = redMaterial;
+        //Debug.Log(this.gameObject.name);
+    }
+}
---
+++ Lights Out/Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour
+{
+    public int width, height;
+    [HideInInspector] public List<GameObject> tilePrefabList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObjects();
+    }
+    
+    void SpawnObjects()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int randomIndex = Random.Range(0, tilePrefabList.Count);
+                GameObject randomTilePrefab = tilePrefabList[randomIndex];
+                Vector3 spawnPosition = new Vector3(x, y, 0);
+
+                //Instantiate(randomTilePrefab, spawnPosition, Quaternion.identity, this.transform);
+                //GameObject newTileObject = Instantiate(randomTilePrefab, spawnPosition, Quaternion.identity);
+                //newTileObject.transform.SetParent(this.transform);
+
+                GameObject newTileObject = Instantiate(randomTilePrefab, this.transform);
+                newTileObject.transform.localPosition = spawnPosition;
+            }
+        }
+    }
+}
